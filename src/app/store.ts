3bca30ae0b6f8b4800e32342d2c154602851838f
Steven <stevenle18@gmail.com>
2021-05-07 01:55:47 -0700

    feat(results) display movies fetched from api

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import { moviesReducer } from '../features/Movies/MoviesReducer';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     movies: moviesReducer
   },
 });
